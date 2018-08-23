e539ddb50ed924087c70027e83107e7f3c1b2efa
Anni <32060255+eneaneli@users.noreply.github.com>
2018-08-23 09:26:01 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day4VideoMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
